72bcae5d2cc00951f0fb1f9f79c43ab4704d1f2e
Yomi Ikuru <abayomi.ikuru@gmail.com>
2021-05-18 12:07:02 +0100

    Add more kucoin API functions for pump process

--- node_wip/src/bot-kucoin.js
+++ node_wip/src/bot-kucoin.js
@@ -1,4 +1,5 @@
 import kucoin from "kucoin-node-sdk";
+import { v4 as uuidv4 } from "uuid";
 
 import { inquirerImportUserDetails } from "./import.js";
 import { inquirerSelectTradeConfig, inquirerInputCoin } from "./prompts.js";
@@ -12,8 +13,9 @@ export default class KucoinBot {
 
   constructor() {
     this.balance;
+    this.tradingAmount;
+    this.buyOrderId;
     this.buyOrder;
-    this.buyInfo;
     this.sellOrder;
     this.sellInfo;
     this.coinPair;
@@ -27,6 +29,52 @@ export default class KucoinBot {
     kucoin.init(this.#userSecrets);
   }
 
+  getTicker() {
+    return kucoin.rest.Market.Symbols.getTicker(this.coinPair);
+  }
+
+  getAccountUsers() {
+    return kucoin.rest.User.UserInfo.getSubUsers();
+  }
+
+  async validateAPIConnection() {
+    const acctInfo = await this.getAccountUsers();
+    if (acctInfo.code !== "200000") {
+      throw "API secrets cannot be validated";
+    }
+  }
+
+  getBalance() {
+    this.balance = kucoin.rest.User.Account.getAccountsList({
+      type: "trade",
+      currency:
+        this.#userConfig["trade_configs"][this.selectedConfig]["pairing"],
+    }).data;
+  }
+
+  getTradingAmount() {
+    this.tradingAmount =
+      parseFloat(this.balance["data"][0]["available"]) *
+      this.#userConfig["trade_configs"][this.selectedConfig][
+        "buy_qty_from_wallet"
+      ];
+  }
+
+  marketBuyOrder() {
+    this.buyOrderId = kucoin.rest.Trade.Orders.postOrder(
+      {
+        clientOid: uuidv4(),
+        side: "buy",
+        symbol: this.coinPair,
+        type: "market",
+      },
+      {
+        size: "",
+      }
+    );
+    // Use orderId and get order details
+  }
+
   // changing a this.value value in an arrow function doesn't change it outside the scope
   // For Arrow functions
 
@@ -37,8 +85,16 @@ export default class KucoinBot {
     );
 
     await this.initialiseAPI();
+    await this.validateAPIConnection();
 
     this.selectedConfig = await inquirerSelectTradeConfig(this.#userConfig);
+
+    // store balance
+    this.getBalance();
+    console.log(await this.balance);
+    // this.getTradingAmount()
+    // console.log(await this.tradingAmount);
+
     this.selectedCoin = await inquirerInputCoin();
 
     this.coinPair =
