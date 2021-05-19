5d4ce29c9c619f481498c5d6fbd14ce197e04c8c
Yomi Ikuru <abayomi.ikuru@gmail.com>
2021-05-19 01:38:11 +0100

    Node kucoin bot now Functional

--- node_wip/src/bot-kucoin.js
+++ node_wip/src/bot-kucoin.js
@@ -1,5 +1,6 @@
 import kucoin from "kucoin-node-sdk";
 import { v4 as uuidv4 } from "uuid";
+import chalk from "chalk"
 
 import { inquirerImportUserDetails } from "./import.js";
 import { inquirerSelectTradeConfig, inquirerInputCoin } from "./prompts.js";
@@ -12,12 +13,14 @@ export default class KucoinBot {
   #userConfig;
 
   constructor() {
-    this.balance;
+    this.quoteCoinBalance;
+    this.baseCoinBalance;
     this.tradingAmount;
+    this.ticker;
     this.buyOrderId;
     this.buyOrder;
+    this.sellOrderId;
     this.sellOrder;
-    this.sellInfo;
     this.coinPair;
     this.selectedConfig;
     this.selectedCoin;
@@ -29,8 +32,8 @@ export default class KucoinBot {
     kucoin.init(this.#userSecrets);
   }
 
-  getTicker() {
-    return kucoin.rest.Market.Symbols.getTicker(this.coinPair);
+  async getTicker() {
+    this.ticker = await kucoin.rest.Market.Symbols.getTicker(this.coinPair);
   }
 
   getAccountUsers() {
@@ -44,24 +47,31 @@ export default class KucoinBot {
     }
   }
 
-  getBalance() {
-    this.balance = kucoin.rest.User.Account.getAccountsList({
+  async getquoteCoinBalance() {
+    this.quoteCoinBalance = await kucoin.rest.User.Account.getAccountsList({
       type: "trade",
       currency:
         this.#userConfig["trade_configs"][this.selectedConfig]["pairing"],
-    }).data;
+    });
+    // console.log(this.quoteCoinBalance);
+    console.log(`Your ${this.#userConfig['trade_configs'][this.selectedConfig]['pairing']} balance is ${this.quoteCoinBalance["data"][0]['available']}\n`);
+    console.log(chalk.yellow("Please check your config before proceeding\n"));
   }
 
-  getTradingAmount() {
-    this.tradingAmount =
-      parseFloat(this.balance["data"][0]["available"]) *
-      this.#userConfig["trade_configs"][this.selectedConfig][
-        "buy_qty_from_wallet"
-      ];
+  async getTradingAmount() {
+    this.tradingAmount = Math.floor(
+      parseFloat(this.quoteCoinBalance["data"][0]["available"]) *
+        this.#userConfig["trade_configs"][this.selectedConfig][
+          "buy_qty_from_wallet"
+        ]
+    );
+    // console.log(typeof this.tradingAmount);
   }
 
-  marketBuyOrder() {
-    this.buyOrderId = kucoin.rest.Trade.Orders.postOrder(
+  async marketBuyOrder() {
+    // current_price = client.get_ticker(symbol=selected_coin_pair)
+    // buy_qty = math.floor(avail_trading_amount / float(current_price['price']))
+    this.buyOrderId = await kucoin.rest.Trade.Orders.postOrder(
       {
         clientOid: uuidv4(),
         side: "buy",
@@ -69,12 +79,104 @@ export default class KucoinBot {
         type: "market",
       },
       {
-        size: "",
+        funds: this.tradingAmount,
       }
     );
-    // Use orderId and get order details
   }
 
+  async getBuyOrderDetails() {
+    // Use orderId and get order details
+    this.buyOrder = await kucoin.rest.Trade.Orders.getOrderByID(
+      this.buyOrderId["data"]["orderId"]
+    );
+    console.log(this.buyOrder["data"]);
+  }
+
+  async getBaseCoinBalance() {
+    this.baseCoinBalance = await kucoin.rest.User.Account.getAccountsList({
+      currency: this.selectedCoin,
+      type: "trade",
+    });
+  }
+
+  async checkMargin() {
+    const margin =
+      this.#userConfig["trade_configs"][this.selectedConfig]["profit_margin"];
+    const interval =
+      this.#userConfig["trade_configs"][this.selectedConfig][
+        "refresh_interval"
+      ];
+
+    this.fallbackAction();
+
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      await this.getTicker();
+      try {
+        if (
+          parseFloat(this.ticker["data"]["price"]) >=
+          (parseFloat(this.buyOrder["data"]["dealFunds"]) /
+            parseFloat(this.buyOrder["data"]["dealSize"])) *
+            (1.0 + margin)
+        ) {
+          if (this.sellOrderId == null) {
+            await this.marketSellOrder();
+            break;
+          }
+        } else {
+          await new Promise((r) => setTimeout(r, interval));
+        }
+      } catch {
+        console.log("Buy order details not received yet");
+      }
+      if (this.sellOrderId) {
+        break
+      }
+    }
+  }
+
+  async fallbackAction() {
+    const sleepDuration =
+      this.#userConfig["trade_configs"][this.selectedConfig][
+        "sell_fallback_timeout_ms"
+      ];
+
+    // await setTimeout. A.K.A sleep
+    await new Promise((r) => setTimeout(r, sleepDuration));
+
+    if (this.sellOrderId == null) {
+      await this.marketSellOrder();
+    }
+  }
+
+  async marketSellOrder() {
+    const sellQty = Math.floor(parseFloat(this.baseCoinBalance["data"][0]['available']) * this.#userConfig['trade_configs'][this.selectedConfig]['sell_qty_from_wallet'])
+    
+    // console.log(this.quoteCoinBalance["data"][0]['available']);
+    // console.log(sellQty);
+
+    this.sellOrderId = await kucoin.rest.Trade.Orders.postOrder(
+      {
+        clientOid: uuidv4(),
+        side: "sell",
+        symbol: this.coinPair,
+        type: "market",
+      },
+      {
+        size: sellQty,
+      }
+    );
+  }
+
+  async getSellOrderDetails() {
+    console.log(this.sellOrderId);
+    this.sellOrder = await kucoin.rest.Trade.Orders.getOrderByID(
+      this.sellOrderId["data"]["orderId"]
+    );
+    console.log(this.sellOrder["data"]);
+  }
+
+  async timer() {}
   // changing a this.value value in an arrow function doesn't change it outside the scope
   // For Arrow functions
 
@@ -90,16 +192,33 @@ export default class KucoinBot {
     this.selectedConfig = await inquirerSelectTradeConfig(this.#userConfig);
 
     // store balance
-    this.getBalance();
-    console.log(await this.balance);
-    // this.getTradingAmount()
-    // console.log(await this.tradingAmount);
+    await this.getquoteCoinBalance();
+    // console.log(this.quoteCoinBalance);
+    await this.getTradingAmount();
 
     this.selectedCoin = await inquirerInputCoin();
 
     this.coinPair =
-      this.selectedCoin.toUpperCase() +
+      (await this.selectedCoin.toUpperCase()) +
       "-" +
       this.#userConfig["trade_configs"][this.selectedConfig]["pairing"];
+
+    // await this.getTicker();
+    // console.log(await this.ticker);
+
+    await this.marketBuyOrder();
+
+    this.getBuyOrderDetails();
+    // await this.getBuyOrderDetails();
+
+    this.getBaseCoinBalance();
+    // await this.getBaseCoinBalance();
+
+    await this.checkMargin();
+    await this.getSellOrderDetails();
+
+    // Insert into db
+    // Print Balance
+    // Print time duration
   }
 }
