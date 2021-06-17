90bcd085751c13973ba92c54debaa19796def072
Yomi Ikuru <abayomi.ikuru@gmail.com>
2021-06-17 20:10:29 +0100

    Add keymap file

---
+++ node_wip/src/io/keymap.js
@@ -0,0 +1,9 @@
+export const keyMap = {
+    "1": "sell_all",
+    "2": "sell_half",
+    "3": "sell_quarter",
+    "0": "manual_toggle",
+    "\x04": "panic_sell"
+}
+
+// console.log(keyMap["\x04"])
